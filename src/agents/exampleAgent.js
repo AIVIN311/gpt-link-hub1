b8de10a7b082c868f59daec267e982ee3f669c40
Eastily <wayne_minn@icloud.com>
2025-07-16 20:29:05 +1000

    feat: add header component and stub agent

---
+++ src/agents/exampleAgent.js
@@ -0,0 +1,14 @@
+/**
+ * Basic agent module structure
+ */
+
+export default class ExampleAgent {
+  constructor(options = {}) {
+    this.options = options
+  }
+
+  run(input) {
+    // TODO: implement agent logic
+    return { input, output: null }
+  }
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+        <p>Welcome to GPT Link Hub</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
