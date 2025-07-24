eec991bc26bdd492c15c854cf424abce82d022b9
Eastily <wayne_minn@icloud.com>
2025-07-24 20:01:10 +1000

    style: remove hover styles

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -31,7 +31,7 @@ function PreviewCard({ title, description, tags = [], url }) {
         href={url}
         target="_blank"
         rel="noopener noreferrer"
-        className="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        className="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded"
       >
         前往連結
       </a>
